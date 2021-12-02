28a2555c267b6476d402f361af9f90eb794d5db3
Yoon-Jo Hwang <mymelody112266@gmail.com>
2021-12-03 05:16:46 +0900

    change make_new_song 3

--- UI/examples/Make_new_song.py
+++ UI/examples/Make_new_song.py
@@ -18,6 +18,10 @@ from pyqt5Custom import ToggleSwitch, StyledButton, ColorPicker, ColorPreview, D
 from Searchfile import Searchfile
 from inference_ui import infer_test
 
+from input_config import input_config
+from config_parser import Config
+# from training_ui import train_test
+
 class Make_new_song(QDialog):
     def __init__(self, switchWidget):
         super(Make_new_song, self).__init__()
@@ -25,6 +29,9 @@ class Make_new_song(QDialog):
         self.setMinimumSize(150, 37)
         self.setGeometry(100, 100, 890, 610)
         self.switchWidget = switchWidget
+
+        self.config = Config([os.path.join(os.path.dirname(__file__), "../bridge/config/default_train.yml")])
+        self.configWidget = input_config('train', self.config)
 
         self.setAutoFillBackground(True)
         p = self.palette()
@@ -44,14 +51,8 @@ class Make_new_song(QDialog):
         self.switchButton.setSpacing(50)
         self.layout.addLayout(self.conlyt)
         self.layout.addLayout(self.switchButton)
-
-        self.next = StyledButton("Next")
-        self.back = StyledButton("Back")
-        self.back.clicked.connect(lambda: self.switchpage(0))
-        self.next.clicked.connect(lambda: self.switchpage(3))
-
         h = QLabel(
-            "<span style='font-size:48px; font-family:SF Pro Display; color:rgb(28,28,30);'>Making new songs</span>")
+            "<span style='font-size:48px; font-family:SF Pro Display; color:rgb(28,28,30);'>\U0001F399 Making new songs</span>")
         ah = QLabel(
             "<span style='font-size:24px; font-family:SF Pro Display; color:rgb(89,89,92);'>기존 모델들을 바탕으로 새로운 노래 생성</span>")
 
@@ -59,8 +60,19 @@ class Make_new_song(QDialog):
         self.conlyt.addWidget(ah)
         self.conlyt.addSpacing(90)
 
+        self.next = StyledButton("Config setting")
+        self.back = StyledButton("Back")
+        #self.ibtnl = StyledButton("Making New Songs", icon=Spinner(1.5, QColor(0, 255, 255)))
+
+        #self.start = StyledButton("Training Start")
+        self.back.clicked.connect(lambda: switchWidget(0))
+        #self.ibtnl.clicked.connect(infer_test)
+        #self.next.clicked.connect(self.configWidget.load)
+        #self.start.clicked.connect(train_test)
+
         self.switchButton.addWidget(self.back)
         self.switchButton.addWidget(self.next)
+        #self.switchButton.addWidget(self.start)
 
         self.list = QHBoxLayout()
         self.list.setSpacing(15)
@@ -97,49 +109,61 @@ class Make_new_song(QDialog):
             }
         }
 
-        self.back.setFixedSize(90, 44)
+        button_style2 = {
+            'normal': {
+                "background-color": (255, 255, 255),
+                "border-color": (154, 84, 237),
+                "border-radius": 7,
+                "color": (154, 84, 237),
+                "font-family": "SF Pro Display",
+                "font-size": 18,
+            },
+            'hover': {
+                "background-color": (154, 84, 237),
+                "border-color": (154, 84, 237),
+                "color": (255, 255, 255),
+                "font-size": 18,
+            },
+            'press': {
+                "background-color": (154, 84, 237),
+                "border-color": (154, 84, 237),
+                "color": (255, 255, 255),
+                "font-size": 18,
+            }
+        }
+
+        self.back.setFixedSize(100, 54)
         self.back.anim_press.speed = 7.3
-        self.back.setStyleDict({
-            "background-color": (0, 0, 0),
-            "border-color": (0, 0, 0),
-            "border-radius": 7,
-            "color": (255, 255, 255),
-            "font-family": "SF Pro Display",
-            "font-size": 18,
-        })
+        self.back.setStyleDict(button_style2['normal'], "default")
+        self.back.setStyleDict(button_style2['hover'], "hover")
+        self.back.setStyleDict(button_style2['press'], "press")
 
-        self.back.setStyleDict({
-            "background-color": (255, 255, 255),
-            "border-color": (0, 0, 0),
-            "color": (0, 0, 0),
-        }, "hover")
-
-        self.back.setStyleDict({
-            "background-color": (255, 255, 255),
-            "border-color": (0, 0, 0),
-            "color": (0, 0, 0),
-        }, "press")
-
-        self.next.setFixedSize(90, 44)
+        self.next.setFixedSize(120, 54)
         self.next.anim_press.speed = 7.3
-        self.next.setStyleDict({
-            "background-color": (0, 0, 0),
-            "border-color": (0, 0, 0),
-            "border-radius": 7,
-            "color": (255, 255, 255),
+        self.next.setStyleDict(button_style2['normal'], "default")
+        self.next.setStyleDict(button_style2['hover'], "hover")
+        self.next.setStyleDict(button_style2['press'], "press")
+        """
+        self.ibtnl = StyledButton("Making New Songs", icon=Spinner(1.5, QColor(0, 255, 255)))
+        self.ibtnl.setFixedSize(200, 47)
+        self.ibtnl.anim_press.speed = 7.3
+
+        self.ibtnl.setStyleDict(button_style2['normal'], "default")
+        self.ibtnl.setStyleDict(button_style2['hover'], "hover")
+        self.ibtnl.setStyleDict(button_style2['press'], "press")
+        self.ibtnl.clicked.connect(infer_test)
+
+        self.btnlyt2.addSpacing(15)
+        self.btnlyt2.addWidget(self.ibtnl, alignment=Qt.AlignHCenter)
+
+        self.toast = Toast(self, text="Making new Songs", icon=Spinner(1.3, QColor(255, 255, 255)))
+        self.toast.setFixedWidth(287)
+        self.toast.setStyleDict({
             "font-family": "SF Pro Display",
-            "font-size": 18,
+            "font-size": 17
         })
-        self.next.setStyleDict({
-            "background-color": (255, 255, 255),
-            "border-color": (0, 0, 0),
-            "color": (0, 0, 0),
-        }, "hover")
-        self.next.setStyleDict({
-            "background-color": (255, 255, 255),
-            "border-color": (0, 0, 0),
-            "color": (0, 0, 0),
-        }, "press")
+        self.ibtnl.clicked.connect(lambda: self.toast.rise(3))
+        """
 
         self.label1 = QLabel('', self)
         self.text1 = QLabel(
@@ -150,7 +174,7 @@ class Make_new_song(QDialog):
         self.btn1.setStyleDict(button_style['normal'])
         self.btn1.setStyleDict(button_style['hover'], "hover")
         self.btn1.setStyleDict(button_style['press'], "press")
-        #self.btnl.clicked.connect(infer_test)
+        #self.btn1.clicked.connect(lambda: self.default_checkpoint(self.label1))
         self.btn1.clicked.connect(lambda: self.fileSearch(self.label1))
         self.btn1.setContentsMargins(0, 3, 0, 0)
         self.text1.setContentsMargins(20, 3, 0, 0)
@@ -218,167 +242,14 @@ class Make_new_song(QDialog):
         self.btnlyt2.setSpacing(16)
         self.btnslyt.addLayout(self.btnlyt2)
 
-        """
 
-        self.labeloc1 = QVBoxLayout()
-        self.labeloc1.setSpacing(30)
-        self.labeloc1.setContentsMargins(100, 30, 0, 30)
-
-
-        self.btnlyt2.setAlignment(Qt.AlignTop)
-        self.btnlyt2.addWidget(QLabel(
-            "<span style='font-size:17px; font-family:SF Pro Display; color:rgb(99,99,102);'>Choosing Model</span>"))
-        self.btnlyt2.addSpacing(10)
-        
-        
-
-
-        
-        self.segbg = SegmentedButtonGroup(radio=True)
-        self.segbg.setFixedSize(349, 36)
-        self.segbg.setStyleDict({
-            "background-color": (255, 255, 255),
-            "border-color": (154, 84, 237),
-            "border-radius": 7,
-            "color": (154, 84, 237),
-            "font-family": "SF Pro Display",
-            "font-size": 15,
-            "font-subpixel-aa": True
-        })
-        self.segbg.setStyleDict({
-            "background-color": (154, 84, 237),
-            "color": (255, 255, 255),
-        }, "hover")
-        self.segbg.setStyleDict({
-            "background-color": (154, 84, 237),
-            "color": (255, 255, 255),
-        }, "press")
-        self.segbg.setStyleDict({
-            "background-color": (154, 84, 237),
-            "color": (255, 255, 255),
-        }, "check-hover")
-
-        self.labeloc1.addWidget(self.label1, alignment=Qt.AlignHCenter | Qt.AlignBottom)
-
-        self.segbg.addButton("Default")
-        #self.segbg.addButton(self)
-        self.segbg.addButton("Another option")
-        #    self.segbg.addButton("Third")
-
-        #self.segbg.clicked.connect(lambda: self.fileSearch(self.label1))
-
-        #self.segbg[0].clicked.connect(lambda : self.fileSearch(self.label1))
-        self.btnlyt2.addWidget(self.segbg)
-        """
-
-        """
-
-        self.btnlyt2.addSpacing(10)
-        self.btnlyt2.addWidget(QLabel(
-            "<span style='font-size:17px; font-family:SF Pro Display; color:rgb(99,99,102);'>Lyrics</span>"))
-        self.btnlyt2.addSpacing(10)
-
-        self.ibtnlyt = QHBoxLayout()
-        self.btnlyt2.addWidget(QLabel(
-            "<span style='font-size:17px; font-family:SF Pro Display; color:rgb(99,99,102);'>SheetMusic</span>"))
-        self.btnlyt2.addSpacing(10)
-        self.btnlyt2.addLayout(self.ibtnlyt)
-
-        self.labeloc2 = QHBoxLayout()
-        self.labeloc2.setSpacing(100)
-        self.labeloc2.setContentsMargins(100, 30, 0, 30)
-
-        self.labeloc2.addWidget(self.label2, alignment=Qt.AlignHCenter | Qt.AlignBottom)
-        self.ibtn = StyledButton("Find", icon="data/TTS.png")
-
-        self.ibtn.setFixedSize(140, 45)
-        self.ibtn.anim_press.speed = 7.3
-
-        #self.layout.addWidget(self.label2)
-        #self.layout.addWidget(self.ibtn)
-
-
-        #self.ibtn.clicked.connect(self.add_open)
-        self.ibtn.clicked.connect(lambda: self.fileSearch(self.label2))
-
-        self.ibtn.setStyleDict({
-            "background-color": (154, 84, 237),
-            "border-color": (154, 84, 237),
-            "border-radius": 7,
-            "color": (255, 255, 255),
-            "font-family": "SF Pro Display",
-            "font-size": 18,
-        })
-
-        self.ibtn.setStyleDict({
-            "background-color": (102, 71, 214),
-            "border-color": (102, 71, 214)
-        }, "hover")
-
-        self.ibtn.setStyleDict({
-            "background-color": (102, 71, 214),
-            "border-color": (102, 71, 214),
-            "color": (255, 255, 255),
-        }, "press")
-
-
-        self.ibtnlyt.addWidget(self.ibtn)
-
-        self.labeloc2.addWidget(self.label3, alignment=Qt.AlignCenter | Qt.AlignCenter)
-        self.ibtn2 = StyledButton("Find", icon="data/TTS.png")
-        self.ibtn2.setFixedSize(140, 45)
-        self.ibtn2.anim_press.speed = 7.3
-
-        #self.ibtn2.clicked.connect(lambda: {self.add_open})
-        #self.ibtn2.clicked.connect(self.add_open)
-        self.ibtn2.clicked.connect(lambda: self.fileSearch(self.label3))
-
-        self.ibtn2.setStyleDict({
-            "background-color": (154, 84, 237),
-            "border-color": (154, 84, 237),
-            "border-radius": 7,
-            "color": (255, 255, 255),
-            "font-family": "SF Pro Display",
-            "font-size": 18,
-        })
-
-        self.ibtn2.setStyleDict({
-            "background-color": (102, 71, 214),
-            "border-color": (102, 71, 214)
-        }, "hover")
-        self.ibtn2.setStyleDict({
-            "background-color": (102, 71, 214),
-            "border-color": (102, 71, 214),
-            "color": (255, 255, 255),
-        }, "press")
-        
-        
-
-
-        self.ibtnlyt.addWidget(self.ibtn2)
-        """
-
-        self.ibtnl = StyledButton("Making New Songs", icon=Spinner(1.5, QColor(255, 255, 255)))
+        self.ibtnl = StyledButton("Making New Songs", icon=Spinner(1.5, QColor(0, 255, 255)))
         self.ibtnl.setFixedSize(200, 47)
         self.ibtnl.anim_press.speed = 7.3
-        self.ibtnl.setStyleDict({
-            "background-color": (154, 84, 237),
-            "border-color": (154, 84, 237),
-            "border-radius": 39,
-            "color": (255, 255, 255),
-            "font-family": "SF Pro Display",
-            "font-size": 18,
-        })
 
-        self.ibtnl.setStyleDict({
-            "background-color": (102, 71, 214),
-            "border-color": (102, 71, 214)
-        }, "hover")
-        self.ibtnl.setStyleDict({
-            "background-color": (102, 71, 214),
-            "border-color": (102, 71, 214),
-            "color": (255, 255, 255),
-        }, "press")
+        self.ibtnl.setStyleDict(button_style2['normal'], "default")
+        self.ibtnl.setStyleDict(button_style2['hover'], "hover")
+        self.ibtnl.setStyleDict(button_style2['press'], "press")
 
         self.ibtnl.clicked.connect(infer_test)
 
@@ -399,8 +270,22 @@ class Make_new_song(QDialog):
         filename = Searchfile.add_open(self, filename)
         labelName.setText(filename)
 
-    #def default_test(self):
-     #   config.checkpoint_file = '..'
+    """
+    
+
+    def default_checkpoint(self, labelName):
+        config.checkpoint_file = '..\\..\\..\\pretrained_sample.pt'
+        labelName.setText(config.checkpoint_file)
+
+    
+
+    def CheckPointSearch(self, labelName):
+        filename = ""
+        #filename = Searchfile.add_open(self, filename)
+        filename = QFileDialog.getOpenFileName(self,'Load Checkpoint', '..\\..\\..\\', filter = '*.pt')
+        labelName.setText(filename)
+        
+    """
 
     def switchpage(self, num):
         self.switchWidget(num)
